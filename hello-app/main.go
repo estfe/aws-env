21b73dfcabb2c247ffb239cc2c2e8b423249967e
Felipe Estrella <felestrella@gmail.com>
2025-05-30 14:38:32 -0300

    Improving go application

--- hello-app/main.go
+++ hello-app/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello world! YAY")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Hello world! YAY")
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
 }
