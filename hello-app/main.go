c62d9d8536b60cc853b940b2a03524cb742c9f33
Felipe Estrella <43611852+estfe@users.noreply.github.com>
2025-05-31 16:52:21 -0300

    Update main.go

--- hello-app/main.go
+++ hello-app/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Wello World, yay2")
+		fmt.Fprintln(w, "Wello World, yay3")
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
