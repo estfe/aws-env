2dbd32f77cfc5ec98204d9e3ff8aef915d1ec2bf
Felipe Estrella <felestrella@gmail.com>
2025-05-31 16:44:00 -0300

    fix endpoint access

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -17,7 +17,7 @@ module "eks" {
   vpc-cni                = {}
   }
 
-  cluster_endpoint_public_access = false
+  cluster_endpoint_public_access = true
   enable_cluster_creator_admin_permissions = true
 
   eks_managed_node_groups = {
