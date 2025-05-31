d3a2486f7dac545b66319be223b9459776958a9e
Felipe Estrella <felestrella@gmail.com>
2025-05-31 19:33:16 -0300

    Terraform fmt command

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -1,31 +1,31 @@
 module "eks" {
-  source = "terraform-aws-modules/eks/aws"
-  version = "20.34.0"  
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.34.0"
 
   #depends_on = [ module.vpc, module.iam ]
 
-  cluster_name = "eks-cluster"
+  cluster_name    = "eks-cluster"
   cluster_version = "1.31"
 
-  vpc_id = var.vpc_id
+  vpc_id     = var.vpc_id
   subnet_ids = var.subnet_ids # Adding the nodes under the private subnet
 
   cluster_addons = {
-  coredns                = {}
-  eks-pod-identity-agent = {}
-  kube-proxy             = {}
-  vpc-cni                = {}
+    coredns                = {}
+    eks-pod-identity-agent = {}
+    kube-proxy             = {}
+    vpc-cni                = {}
   }
 
-  cluster_endpoint_public_access = true
+  cluster_endpoint_public_access           = true
   enable_cluster_creator_admin_permissions = true
 
   eks_managed_node_groups = {
     nodegroup = {
-        instance_types = ["c3.xlarge"]
-        max_size = 3
-        min_size = 1
-        desired_size = 3
+      instance_types = ["c3.xlarge"]
+      max_size       = 3
+      min_size       = 1
+      desired_size   = 3
     }
   }
 
