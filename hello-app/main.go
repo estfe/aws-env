82f392b68a7114fd2b9502d79e0b6afd8c1ef630
Felipe Estrella <43611852+estfe@users.noreply.github.com>
2025-06-01 19:10:47 -0300

    Update main.go

--- hello-app/main.go
+++ hello-app/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Wello World2")
+		fmt.Fprintln(w, "Wello World3")
 	})
 
 	fmt.Println("Hello world")
