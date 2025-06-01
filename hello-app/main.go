182078b8ee2e3ee42aa696aa127387df93e3c58d
Felipe Estrella <felestrella@gmail.com>
2025-06-01 12:05:39 -0300

    Generate logs

--- hello-app/main.go
+++ hello-app/main.go
@@ -9,8 +9,9 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Wello World")
-		fmt.Println("Hello world")
 	})
+
+	fmt.Println("Hello world")
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
